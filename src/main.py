eab5024e865c329f695a1543a060b2394e8a060f
josiah <josiah.fok@dewc.com>
2025-10-15 01:48:09 +0000

    Clean start

--- src/main.py
+++ src/main.py
@@ -17,18 +17,4 @@ app = FastAPI()
 def testGet():
     return "HIIIII"
 
-@app.get("/gimmefroot/{frootname}")
-def gimmeDat(frootname: fruits):
-    if frootname is fruits.apple:
-        return "You tryna keep the doctor away huh!!"
-    elif frootname is fruits.pear:
-        return "You nasty ahh for eating dis stuff..."
-    elif frootname is fruits.banana:
-        return "Based fruit choice"
-    else:
-        return "You're not an average fruit enjoyer are you?"
-    
-@app.post("/newfroot/")
-async def create_froot(item: Item):
-    item.name = item.name.capitalize()
-    return item
+
